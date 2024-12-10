6dbfdea2150c94938932792225079d1c1b19ca64
Nathan Barnes <339723215@GoTVDSB.ca>
2024-12-10 09:22:27 -0500

    Add project files.

---
+++ Collision detection/Game1.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Collision_detection
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        KeyboardState keyboardState;
+        MouseState mouseState;
+        // Textures
+        Texture2D pacLeftTexture;
+        Texture2D pacRightTexture;
+        Texture2D pacUpTexture;
+        Texture2D pacDownTexture;
+        Texture2D currentPacTexture; // Current Pacman texture to draw
+        Rectangle pacRect; // This rectangle will track where Pacman is and his size
+        Texture2D exitTexture;
+        Rectangle exitRect;
+        Texture2D barrierTexture;
+        Rectangle barrierRect1, barrierRect2; List<Rectangle> barriers;
+        Texture2D coinTexture;
+        Rectangle coinRect; List<Rectangle> coins;
+        Rectangle window;
+        Vector2 pacSpeed;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            pacSpeed = Vector2.Zero;
+            pacRect = new Rectangle(10, 10, 60, 60);
+            barriers = new List<Rectangle>();
+            barriers.Add(new Rectangle(0, 250, 350, 75));
+            barriers.Add(new Rectangle(450, 250, 350, 75));
+            exitRect = new Rectangle(700, 380, 100, 100);
+            window = new Rectangle(0, 0, 800, 480);
+            base.Initialize();
+
+            coins = new List<Rectangle>();
+            coins.Add(new Rectangle(400, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 300, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 300, coinTexture.Width, coinTexture.Height));
+
+
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Pacman
+            pacDownTexture = Content.Load<Texture2D>("pacDown");
+            pacUpTexture = Content.Load<Texture2D>("pacUp");
+            pacRightTexture = Content.Load<Texture2D>("pacRight");
+            pacLeftTexture = Content.Load<Texture2D>("pacLeft");
+            currentPacTexture = pacRightTexture;
+            // Barrier
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
+            // Exit
+            exitTexture = Content.Load<Texture2D>("door");
+            // Coin
+            coinTexture = Content.Load<Texture2D>("coin");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            keyboardState = Keyboard.GetState();
+
+            pacSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacSpeed.X -= 2;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacSpeed.X += 2;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacSpeed.Y -= 2;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacSpeed.Y += 2;
+                currentPacTexture = pacDownTexture;
+            }
+            pacRect.Offset(pacSpeed);
+
+            for (int i = 0; i < coins.Count; i++)
+            {
+                if (pacRect.Intersects(coins[i]))
+                {
+                    coins.RemoveAt(i);
+                    i--;
+                }
+            }
+            mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+                if (exitRect.Contains(mouseState.X, mouseState.Y))
+                    Exit();
+
+            if (exitRect.Contains(pacRect))
+                Exit();
+
+            foreach (Rectangle barrier in barriers)
+                if (pacRect.Intersects(barrier))
+                    pacRect.Offset(-pacSpeed);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            foreach (Rectangle barrier in barriers)
+                _spriteBatch.Draw(barrierTexture, barrier, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            foreach (Rectangle coin in coins)
+                _spriteBatch.Draw(coinTexture, coin, Color.White);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
